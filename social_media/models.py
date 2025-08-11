b22e2b754e3990bc8cbe979bef65692325970b33
Danylo Skepskyi <kol230305@gmail.com>
2025-08-11 11:16:35 +0200

    Dev: added social_media models, added requirements.txt

--- social_media/models.py
+++ social_media/models.py
@@ -1,57 +1,86 @@
+import os
 import uuid
 
-from django.contrib.auth.models import (
-    AbstractUser,
-    BaseUserManager,
-)
 from django.db import models
+
+from social_media_api import settings
 from django.utils.translation import gettext_lazy as _
 
 
-# noinspection PySimplifyBooleanCheck
-class UserManager(BaseUserManager):
-    """Define a model manager for User model with no username field."""
-
-    use_in_migrations = True
-
-    def _create_user(self, email, password, **extra_fields):
-        """Create and save a User with the given email and password."""
-        if not email:
-            raise ValueError("The given email must be set")
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_user(self, email, password=None, **extra_fields):
-        """Create and save a regular User with the given email and password."""
-        extra_fields.setdefault("is_staff", False)
-        extra_fields.setdefault("is_superuser", False)
-        return self._create_user(email, password, **extra_fields)
-
-    def create_superuser(self, email, password, **extra_fields):
-        """Create and save a SuperUser with the given email and password."""
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError("Superuser must have is_staff=True.")
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError("Superuser must have is_superuser=True.")
-
-        return self._create_user(email, password, **extra_fields)
+def post_image(instance: "Post", filename: str) -> str:
+    ext = filename.split(".")[-1]
+    filename = f"{uuid.uuid4()}.{ext}"
+    return os.path.join("images", "posts", filename)
 
 
-class User(AbstractUser):
+class BaseModel(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    username = None
-    email = models.EmailField(_("Email address."), unique=True)
 
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
+    class Meta:
+        abstract = True
 
-    objects = UserManager()
+
+class PostImage(BaseModel):
+    image = models.ImageField(
+        upload_to=post_image,
+    )
+    post = models.ForeignKey("Post", on_delete=models.CASCADE)
+    position = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        ordering = ["position"]
+        constraints = [
+            models.UniqueConstraint(
+                fields=["post", "position"],
+                name="unique_post_position",
+            )
+        ]
+        verbose_name_plural = _("Post Images")
+        verbose_name = _("Post Image")
 
     def __str__(self):
-        return self.email
+        return f"{self.post.pk} - {self.position}"
+
+
+class Comment(BaseModel):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="comments"
+    )
+    parent = models.ForeignKey(
+        "self", null=True, blank=True, related_name="replies", on_delete=models.CASCADE
+    )
+    text = models.CharField(max_length=255)
+    post = models.OneToOneField("Post", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    likes = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name="comment_likes", blank=True
+    )
+
+    class Meta:
+        ordering = ["-created_at"]
+        verbose_name_plural = _("Comments")
+        verbose_name = _("Comment")
+
+    def __str__(self):
+        return f"{self.user} comment for {self.post.pk}"
+
+
+class Post(BaseModel):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="posts"
+    )
+    likes = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name="liked_posts", blank=True
+    )
+    description = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+        verbose_name_plural = _("Posts")
+        verbose_name = _("Post")
+
+    def __str__(self):
+        return f"{self.user.username} post #{self.created_at}"
