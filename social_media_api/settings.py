aa72dddb68658b0316ae3a1e7c27e9cc5048cf27
Danylo Skepskyi <kol230305@gmail.com>
2025-08-14 16:05:32 +0200

    Dev: fixed cache error

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -237,12 +237,4 @@ CELERY_TASK_TIME_LIMIT = 30 * 60
 
 CELERY_RESULT_BACKEND = "django-cache"
 
-CELERY_CACHE_BACKEND = "default"
 CELERY_BROKER_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
-
-CACHES = {
-    "default": {
-        "BACKEND": "django.core.cache.backends.db.DatabaseCache",
-        "LOCATION": "my_cache_table",
-    }
-}
